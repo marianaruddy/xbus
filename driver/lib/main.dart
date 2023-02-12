5302b61b31baacce6eaa5bfc1d2964577ebdbd12
Mariana Ruddy <marianamrsantos@gmail.com>
2023-02-12 10:25:24 -0300

    connect to firebase

--- driver/lib/main.dart
+++ driver/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:driver/screens/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+bool shouldUseFirebaseEmulator = false;
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
