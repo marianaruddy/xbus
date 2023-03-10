3bd24b8d92b28f241a8d215d6a41025ddfd6bd8f
Mariana Ruddy <marianamrsantos@gmail.com>
2023-03-10 11:26:29 -0300

    get routes from firebase

---
+++ driver/lib/models/route.dart
@@ -0,0 +1,9 @@
+class RouteModel {
+
+  final String destiny;
+  final int number;
+  final String origin;
+
+  RouteModel ({ required this.destiny, required this.number, required this.origin });
+  
+}
---
+++ driver/lib/screens/home/route_form.dart
@@ -0,0 +1,61 @@
+import 'package:driver/models/route.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:driver/shared/constants.dart';
+
+class RouteForm extends StatefulWidget {
+  const RouteForm({super.key});
+
+  @override
+  State<RouteForm> createState() => _RouteFormState();
+}
+
+class _RouteFormState extends State<RouteForm> {
+  @override
+  Widget build(BuildContext context) {
+
+    final routes = Provider.of<List<RouteModel>?>(context);
+  
+    final _formKey = GlobalKey<FormState>();
+  
+    String? currentRoute;
+
+    return Container(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: 20.0),
+                  routes!.length > 0 ? DropdownButtonFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'Selecione uma rota'),
+                    value: currentRoute,
+                    items: routes.map((route) {
+                      return DropdownMenuItem(
+                        value: route,
+                        child: Text('Linha ${route.number.toString()}: ${route.origin} - ${route.destiny}'),
+                      );
+                    }).toList(), 
+                    onChanged: (value) { setState(() => currentRoute = value as String? ); },
+                  ) : 
+                  Text('Nenhuma rota cadastrada'),
+                  SizedBox(height: 20.0),
+    
+                  // TODO: add google maps integration
+    
+                  // // TODO: image not working
+                  // Image.asset(
+                  //   'assets/map.png',
+                  //   fit: BoxFit.contain,
+                  // ),
+    
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text('Iniciar Viagem'),
+                  )
+                ],
+              ),
+            ),);
+  }
+}
