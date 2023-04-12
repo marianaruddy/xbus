0d04544be1ed9544d4aaa82a70c19380b3bfab88
Mariana Ruddy <marianamrsantos@gmail.com>
2023-04-12 18:04:39 -0300

    refac

--- driver/lib/screens/Page2.dart
+++ driver/lib/screens/Page2.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key});
