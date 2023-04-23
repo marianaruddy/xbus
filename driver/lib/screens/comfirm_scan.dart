cfb2b1c9da71da06b0ddaa19f8922bc4191f88ba
Mariana Ruddy <marianamrsantos@gmail.com>
2023-04-23 16:57:03 -0300

    add timer to return

--- driver/lib/screens/comfirm_scan.dart
+++ driver/lib/screens/comfirm_scan.dart
@@ -1,7 +1,5 @@
-import 'package:driver/models/ticket.dart';
-import 'package:driver/services/database.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ConfirmScan extends StatelessWidget {
   String? code;
@@ -12,12 +10,13 @@ class ConfirmScan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    Timer(const Duration(milliseconds: 3000), () => Navigator.pop(context));
+
     return  Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-        ),
-        body: Text('ticket scaneado com sucesso: \n$code'),
-      
+      appBar: AppBar(
+        elevation: 0.0,
+      ),
+      body: Text('ticket scaneado com sucesso: \n$code'),
     );
   }
 }
--- driver/lib/screens/qrcode.dart
+++ driver/lib/screens/qrcode.dart
@@ -1,7 +1,7 @@
 import 'package:driver/models/ticket.dart';
 import 'package:driver/screens/comfirm_scan.dart';
 import 'package:driver/services/database.dart';
-import 'package:flutter/foundation.dart';
+import 'package:driver/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
@@ -80,47 +80,51 @@ class _QRCodePageBodyState extends State<QRCodePageBody> {
   @override
   Widget build(BuildContext context) {
   
-    final tickets = Provider.of<List<Ticket>?>(context) ?? [];
+    List<Ticket> tickets = Provider.of<List<Ticket>?>(context) ?? [];
 
     print('tickets $tickets');
+    if (tickets.length < 1) {
+      return Loading();
+    }
+    else {
 
     return MobileScanner(
-          // fit: BoxFit.contain,
-          controller: cameraController,
-          onDetect: (capture) {
-            final List<Barcode> barcodes = capture.barcodes;
-            final Uint8List? image = capture.image;
-            for (final barcode in barcodes) {
-              debugPrint('Barcode found! ${barcode.rawValue}');
+      // fit: BoxFit.contain,
+      controller: cameraController,
+      onDetect: (capture) async {
+        final List<Barcode> barcodes = capture.barcodes;
+        for (final barcode in barcodes) {
+          debugPrint('Barcode found! ${barcode.rawValue}');
+        }
+        cameraController.stop();
+        String? code = barcodes[0].rawValue;
+        bool isTicketChecked = false;
+        Ticket? scannedTicket;
+        for (Ticket t in tickets) {
+          if (t.id == code) {
+            scannedTicket = t;
+            if(scannedTicket.checked == false) {
+              await DatabaseService().updateTicket(code, {
+                'Checked': true,
+              });
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return ConfirmScan(barcodes[0].rawValue);
+                }),
+              ).then((value) => cameraController.start());
             }
-            cameraController.stop();
-            String? code = barcodes[0].rawValue;
-            try {
-              bool isTicketChecked = false;
-              Ticket? scannedTicket;
-              for (Ticket t in tickets) {
-                if (t.id == code) {
-                  scannedTicket = t;
-                  if(scannedTicket.checked == false) {
-                    DatabaseService().updateTicket(code, {
-                      'Checked': true,
-                    });
-                  }
-                  else {
-                    throw Exception('Ticket já usado');
-                  }
-                }
-              }              
-            } catch (e) {
-              print('erro: $e');
+            else {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return ConfirmScan('ERROR: Ticket já usado');
+                }),
+              ).then((value) => cameraController.start());
             }
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) {
-                return ConfirmScan(barcodes[0].rawValue);
-              }),
-            ).then((value) => cameraController.start());
-          },
-        );
+          }
+        }
+      });
+    }
   }
 }
