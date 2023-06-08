afe08c14844b21116035e6ca8d3c0f93f645743f
Mariana Ruddy <marianamrsantos@gmail.com>
2023-06-08 13:18:42 -0300

    refac

--- driver/lib/services/vehicle.dart
+++ driver/lib/services/vehicle.dart
@@ -4,26 +4,28 @@ import 'package:driver/models/vehicle.dart';
 class VehicleService {
   final CollectionReference vehicleCollection = FirebaseFirestore.instance.collection('Vehicle');
 
+  Vehicle createVehicleInstance(doc) {
+    String licensePlate;
+    int capacity;
+    if ((doc.data() as Map<String, dynamic>).containsKey('LicensePlate')) {
+      licensePlate = doc['LicensePlate'];
+    } else {
+      licensePlate = '';
+    }
+    if ((doc.data() as Map<String, dynamic>).containsKey('Capacity')) {
+      capacity = doc['Capacity'];
+    } else {
+      capacity = 0;
+    }
+    return Vehicle(
+      id: doc.id,
+      licensePlate: licensePlate,
+      capacity: capacity,
+    );
+  }
+
   List<Vehicle> _vehicleListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.docs.map((doc) {
-      String licensePlate;
-      int capacity;
-      if ((doc.data() as Map<String, dynamic>).containsKey('LicensePlate')) {
-        licensePlate = doc['LicensePlate'];
-      } else {
-        licensePlate = '';
-      }
-      if ((doc.data() as Map<String, dynamic>).containsKey('Capacity')) {
-        capacity = doc['Capacity'];
-      } else {
-        capacity = 0;
-      }
-      return Vehicle(
-        id: doc.id,
-        licensePlate: licensePlate,
-        capacity: capacity,
-      );
-    }).toList();
+    return snapshot.docs.map((doc) => (createVehicleInstance(doc))).toList();
   }
 
   Stream<List<Vehicle>> get vehicles {
